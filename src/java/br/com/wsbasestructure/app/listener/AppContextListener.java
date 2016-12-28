e166f1fda4ab7f03bf2b92ea6fdef983f4668f06
Andrew Ribeiro <Andrew Ribeiro@172.16.0.140>
2016-12-27 23:07:51 -0200

    Fixed AppContextListener package

---
+++ src/java/br/com/wsbasestructure/app/listener/AppContextListener.java
@@ -0,0 +1,46 @@
+package br.com.wsbasestructure.app.listener;
+
+import br.com.wsbasestructure.dao.abstracts.Connection;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ *
+ * @author Andrew Ribeiro
+ */
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+    /**
+     *
+     * @param sce
+     *
+     * This method is executed when the application is up by the first time. It
+     * initiates a new connection to be used by all users in the application.
+     * This connection is set on a appcontext attribute. Then this attribute
+     * could be retrieved by any requester.
+     */
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().setAttribute("hefesto", new Connection()); //set the new connection into a appcontext attribute.
+    }
+
+    /**
+     *
+     * @param sce
+     *
+     * This method will be executed when the application is shutdown. It will
+     * completely finalize all the hibernate connections resources, including
+     * the C3p0 connection pool.
+     */
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        if (sce.getServletContext().getAttribute("hefesto") != null) {
+            //if the appcontext attribute containing the connection is different of null, then lets finalize eveything.
+            Connection c = (Connection) sce.getServletContext().getAttribute("hefesto");
+            c.closeEverything();
+        }
+    }
+
+}
