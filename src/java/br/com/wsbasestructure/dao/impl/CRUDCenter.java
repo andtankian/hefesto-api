18a6c0111e32c58e7c2a413eb4e08bacf934c2d8
Andrew Ribeiro <Andrew Ribeiro@172.16.0.140>
2016-12-28 23:56:12 -0200

    CRUDCenter updated to support Department DAO operations

--- src/java/br/com/wsbasestructure/dao/impl/CRUDCenter.java
+++ src/java/br/com/wsbasestructure/dao/impl/CRUDCenter.java
@@ -1,5 +1,7 @@
 package br.com.wsbasestructure.dao.impl;
 
+import br.com.hefesto.resources.impl.deparment.dao.DepartmentDAO;
+import br.com.hefesto.domain.impl.Department;
 import br.com.wsbasestructure.dao.abstracts.AbstractPersistenceCenter;
 import br.com.wsbasestructure.dao.interfaces.IDAO;
 import br.com.wsbasestructure.dto.interfaces.IHolder;
@@ -13,7 +15,11 @@ public class CRUDCenter extends AbstractPersistenceCenter{
 
     @Override
     public IDAO create(Session session, IHolder holder) {
-       return null;
+       if(holder.getEntities().get(0) instanceof Department){
+           return new DepartmentDAO(session, holder);
+       } else {
+           return null;
+       }
     }
 
     
