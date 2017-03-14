00813437d0014a119735bc1aac0d1c9cd7f8b673
Andrew Ribeiro <Andrew Ribeiro@172.16.0.140>
2017-03-14 14:05:37 -0300

    Buf fixed when an invalid password is passed

--- src/java/br/com/hefesto/resources/impl/user/rules/ValidateLoginUserPasswordCommand.java
+++ src/java/br/com/hefesto/resources/impl/user/rules/ValidateLoginUserPasswordCommand.java
@@ -12,37 +12,38 @@ import org.mindrot.jbcrypt.BCrypt;
  *
  * @author Andrew Ribeiro
  */
-public class ValidateLoginUserPasswordCommand implements ICommand{
+public class ValidateLoginUserPasswordCommand implements ICommand {
 
     User userTrying;
-    public ValidateLoginUserPasswordCommand(User userTrying) {        
+
+    public ValidateLoginUserPasswordCommand(User userTrying) {
         this.userTrying = userTrying;
     }
-    
-    
 
     @Override
     public IHolder exe(IHolder holder, FlowContainer flowContainer) {
-        User u = (User)holder.getEntities().get(0);
+        User u = (User) holder.getEntities().get(0);
         Message m = new Message();
         boolean isValid = true;
-        
-        if(u == null){
+
+        if (u == null) {
             m.setError("user doesn't exist");
             isValid = false;
-        } else if(!BCrypt.checkpw(userTrying.getPassword(), u.getPassword())){
+        } else if (!BCrypt.checkpw(userTrying.getPassword(), u.getPassword())) {
             m.setError("invalid password");
             isValid = false;
+            flowContainer.getResult().setHolder(null);
         }
-        
-        if(!isValid){
+
+        if (!isValid) {
             flowContainer.getResult().setMessage(m);
             flowContainer.getResult().setStatus(Result.ERROR);
             flowContainer.getFc().setMustContinue(false);
+        } else {
+
+            flowContainer.getResult().setHolder(holder);
         }
-        
-        flowContainer.getResult().setHolder(holder);
         return holder;
     }
-    
+
 }
